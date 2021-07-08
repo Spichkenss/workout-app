b10eefc56ca6385e345ed474bf48db12ddb4efa2
Max Shushval <8555234@gmail.com>
2021-07-08 08:26:42 +0300

    Feat(exercise): Add form for create exersice. Add endpoint get exersices.

--- back/controllers/exercise/mainController.js
+++ back/controllers/exercise/mainController.js
@@ -5,12 +5,12 @@ import Exercise from '../../models/exerciseModel.js'
 // @route   POST /api/exercises
 // @access  Private
 export const createNewExercise = asyncHandler(async (req, res) => {
-	const { name, times, imageIndex } = req.body
+	const { name, times, imageName } = req.body
 
 	const exercise = await Exercise.create({
 		name,
 		times,
-		imageIdx: imageIndex,
+		imageName,
 	})
 
 	res.json(exercise)
@@ -20,7 +20,7 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 // @route   PUT /api/exercises
 // @access  Private
 export const updateExercise = asyncHandler(async (req, res) => {
-	const { name, times, imageIndex, exerciseId } = req.body
+	const { name, times, imageName, exerciseId } = req.body
 
 	const exercise = await Exercise.findById(exerciseId)
 
@@ -31,7 +31,7 @@ export const updateExercise = asyncHandler(async (req, res) => {
 
 	exercise.name = name
 	exercise.times = times
-	exercise.imageIdx = imageIndex
+	exercise.imageName = imageName
 
 	const updatedExercise = await exercise.save()
 
@@ -54,4 +54,13 @@ export const deleteExercise = asyncHandler(async (req, res) => {
 	await exercise.remove()
 
 	res.json({ message: 'Exercise has been removed' })
+})
+
+// @desc    Get exercises
+// @route   GET /api/exercises
+// @access  Private
+export const getExercises = asyncHandler(async (req, res) => {
+	const exercises = await Exercise.find({})
+
+	res.json(exercises)
 })
